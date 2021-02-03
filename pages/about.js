e76f77f35886c6d35b814b4a2cb4e14ea3172ff5
James Kim <67247717+jyk524@users.noreply.github.com>
2021-02-03 00:07:39 -0500

    Update: Class component

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { createElement } from 'react';
 
 // functional component - arrow function
-const About = () => {
-    const name = 'James'
-    return (
-        <h1> Hello {name} </h1>
-    )
-}
+// When to use 
+// 1. For smaller components
+// 2. Resusable component
+// 3. presentational components, we can use Hooks and speicify state
+
+// const About = () => {
+//     const name = 'James'
+//     return (
+//         <h1> Hello {name} </h1>
+//     )
+// }
 
 // React.createElement('type of element', state, 'what's inside the element')
 // const About = () => {
@@ -15,12 +20,21 @@ const About = () => {
 // }
 
 
-
 // functional component - normal function
 // function About() {
 //     return (
 //         <h1>Hello About Page</h1>
 //     )
 // }
+
+// Class component
+class About extends React.Component {
+    
+    render() {
+        return (
+            <h1> Hello I am a class component</h1>
+        )
+    }
+}
 
 export default About;
