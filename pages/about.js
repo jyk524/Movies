9348d0b91638bf41689473985a8a15404ba99176
James Kim <67247717+jyk524@users.noreply.github.com>
2021-02-02 23:56:05 -0500

    About Page: Page creation, Test: Functional Components

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+
+// functional component
+const About = () => {
+    const message = 'Hello World'
+    return (
+        <h1> {message} </h1>
+    )
+}
+
+
+// functional component - normal function
+// function About() {
+//     return (
+//         <h1>Hello About Page</h1>
+//     )
+// }
+
+export default About;
